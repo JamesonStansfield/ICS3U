ebe07a200ecb9b30a6e13694ad189eacdd10752a
JamesonStansfield <159183491+JamesonStansfield@users.noreply.github.com>
2024-03-19 14:11:40 -0400

    New Output File

---
+++ Unit01/1.1-Output.js
@@ -0,0 +1,8 @@
+//My Tasks
+console.log("\nApple Inc.\nOne Apple Park Way \nCupertino, CA \n95014 \n+1 408-996-1010");
+
+
+console.log("\nMicrosoft Corporation \nOne Microsoft Way \nRedmond, WA \n98052 \n+1 425-882-8080");
+
+
+console.log("\nx o x\no x o\nx o x");
