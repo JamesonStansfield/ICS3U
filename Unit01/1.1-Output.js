f752268e7e106fe2a41fb22c938c89926ed0b24d
JamesonStansfield <159183491+JamesonStansfield@users.noreply.github.com>
2024-03-21 12:37:31 -0400

    changed xox

--- Unit01/1.1-Output.js
+++ Unit01/1.1-Output.js
@@ -5,4 +5,4 @@ console.log("\nApple Inc.\nOne Apple Park Way \nCupertino, CA \n95014 \n+1 408-9
 console.log("\nMicrosoft Corporation \nOne Microsoft Way \nRedmond, WA \n98052 \n+1 425-882-8080");
 
 
-console.log("\nx o x\no x o\nx o x");
+console.log("\nx  o  x\no  x  o\nx  o  x");
