74f53bc26319101a0446f32cbccff242e64868c3
JamesonStansfield <159183491+JamesonStansfield@users.noreply.github.com>
2024-04-04 13:49:37 -0400

    done

---
+++ Unit01/1.3-Mathematics.js
@@ -0,0 +1,25 @@
+//Part 1: Convert Celsius to Farenheit
+
+let celsius = 100;
+let fahrenheit = 0;
+celsius = 102;
+fahrenheit = (celsius * 9/5) + 32;
+//Output just the fahrenheit #
+console.log(fahrenheit);
+
+//Part 2: Convert Farenheit to Celsius
+
+fahrenheit = 77;
+celsius = (fahrenheit - 32) * 5/9;
+//Output just the celsius #
+console.log(celsius);
+
+//Part 3: Now for some text
+
+console.log("77 degrees Fahrenheit is " + celsius + " degrees celsius.");
+
+//Last Part
+
+// A(-4, 5) B(6, 0)
+let m = (0 - 5)/(6 + 4);
+console.log(m);
