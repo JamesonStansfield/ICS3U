ddeae4301ba48e136cba2ec215ffd4aaa7444999
JamesonStansfield <159183491+JamesonStansfield@users.noreply.github.com>
2024-04-04 13:34:48 -0400

    done

---
+++ Unit01/1.2-Variables.js
@@ -0,0 +1,14 @@
+//Part 1
+
+let length = 10;
+let width = 5;
+let area = length * width;
+console.log("The area is " + area + ".");
+console.log("Area: " + area);
+
+//Part 2
+
+let first_name = "Santa";
+let last_name = "Claws";
+last_name = "Claus";
+console.log("I still beleive in " + first_name + " " + last_name + ".");
