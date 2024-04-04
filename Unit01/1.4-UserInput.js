7325330e225ef44679aa183919c916a23f6b1708
JamesonStansfield <159183491+JamesonStansfield@users.noreply.github.com>
2024-04-04 13:58:33 -0400

    temp

---
+++ Unit01/1.4-UserInput.js
@@ -0,0 +1,7 @@
+//Task
+
+console.log("1.4 - User Input");
+prompt("What year were you born?: ");
+console.log("You entered " + prompt);
+age = 2024 - prompt;
+console.log("You are likely " + age + "Years old.");
