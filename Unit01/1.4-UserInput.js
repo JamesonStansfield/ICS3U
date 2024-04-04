ac9766c0aec2eaf5f3d236fffcb0c81beff5b3ba
JamesonStansfield <159183491+JamesonStansfield@users.noreply.github.com>
2024-04-04 14:06:47 -0400

    done

--- Unit01/1.4-UserInput.js
+++ Unit01/1.4-UserInput.js
@@ -1,7 +1,14 @@
 //Task
 
 console.log("1.4 - User Input");
-prompt("What year were you born?: ");
-console.log("You entered " + prompt);
-age = 2024 - prompt;
+let year = prompt("What year were you born?: ");
+console.log("You entered " + year);
+age = 2024 - year;
 console.log("You are likely " + age + "Years old.");
+let name = prompt("What is your name?");
+console.log("Hi, " + name + ". You were born in " + year + " and you are likely " + age + " years old.");
+console.log("Your name is " + name.length + " letters long");
+
+let fahrenheit = prompt("What is the tempurature in fahrenheit, I will convert it to celsius for you.");
+let celsius = (fahrenheit - 32) * 5/9;
+console.log("The tempurature in celcius is: " + celsius + ".");
