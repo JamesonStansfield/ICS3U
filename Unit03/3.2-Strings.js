b232eaf41b945c738dde6239eb724ae12379a181
Jameson Stansfield <jameson.stansfield@stu.ocsb.ca>
2024-05-03 14:23:29 -0400

    First complete

---
+++ Unit03/3.2-Strings.js
@@ -0,0 +1,34 @@
+const prompt = require('prompt-sync')();
+
+//Lesson Tasks:
+
+//Task #1: Reverse Of Given
+
+function reverse(str) {
+    let arrayStr = str.split("");       //splits up the string into peices, Ex: ['J', 'a', 'm', 'e', 's', 'o', 'n']
+    let revArray = arrayStr.reverse();  //reverses that array  
+    let joinArray = revArray.join("");  //Assembles the array back into a string
+    return joinArray;                   //Returns the string as the function
+}
+//console.log(reverse("Jameson"));      //Function Declaration (Remove comment to run)
+
+
+//Task #2: Dragons And Goblins
+
+function dragons_and_goblins(str) {
+  
+}
+
+dragons_and_goblins("dgdgdg");
+
+
+//Task #3: Adding String
+
+function add(str) {
+
+}
+
+
+
+
+//Task #4: Subtraction
